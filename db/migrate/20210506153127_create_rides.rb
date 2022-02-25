4efe78cccf6f739c664fc3ab8fcb50883f07e39b
Haleem Hamid <hhamidbiz@gmail.com>
2022-02-25 08:38:59 -0800

    commit tree 45a03450c2001aa7b8b61910f76783464b24c436

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,10 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      t.string :name
+
+      t.belongs_to :taxi
+      t.belongs_to :passenger
 
       t.timestamps
     end
